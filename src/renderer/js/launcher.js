16ff056f1c90fd7fa69e66b55a39fcdd3119c97f
lolito32 <lolitogedecito1@gmail.com>
2026-01-23 05:05:52 -0300

    feat: implement glassmorphism UI, new logo, and launcher interface

--- src/renderer/js/launcher.js
+++ src/renderer/js/launcher.js
@@ -1,11 +1,6 @@
-const welcome = document.getElementById('welcome')
-const info = document.getElementById('info')
-
 const init = async () => {
     const username = await window.api.getUsername()
-    welcome.innerText = `Welcome ${username}`
-
-    info.innerText = `This app is using Chrome (v${window.api.chrome()}), Node.js (v${window.api.node()}), and Electron (v${window.api.electron()})`
+    welcome.innerText = `Bienvenido ${username}`
 }
 
 init()
